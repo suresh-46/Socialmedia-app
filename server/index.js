fc884b30f7541687d53f83819c02dedb825133ed
suresh <111875766+suresh-46@users.noreply.github.com>
2023-07-04 00:15:58 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -61,9 +61,12 @@ app.use(express.static('public'))
 app.use('/images', express.static('images'))
 
 // Middleware
+const corsOptions = {
+  origin: "http://localhost:3000"
+}
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
+app.use(cors(corsOptions))
 dotenv.config();
 
 mongoose
