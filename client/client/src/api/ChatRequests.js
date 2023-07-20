27c1123a68d57ffdfa7bdfc1ed5524dd68c8575e
suresh <111875766+suresh-46@users.noreply.github.com>
2023-07-20 13:36:19 +0530

    Update ChatRequests.js

--- client/client/src/api/ChatRequests.js
+++ client/client/src/api/ChatRequests.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://social-media-b8pd.onrender.com' });
 
 export const userChats = (id) =>API.get(`/chat/${id}`)
